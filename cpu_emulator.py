c6414f43ffd35d2508c7b44f2ac2ffb17870cd0c
Selim Öztürk <selimozt2013@gmail.com>
2025-09-24 20:51:35 +0300

    ALU unit has added

--- cpu_emulator.py
+++ cpu_emulator.py
@@ -21,15 +21,13 @@ class Memory:
         self.sp+=n
         self.cpu.registers[rsp]=self.sp
         return data
-        
+
     def write_to_heap(self,rax,rdi):
         if rdi<0:
-            return -1
+            return None
         else:
             self.hp+=rdi
             self.cpu.registers[rax]=self.hp
-            return 0
-
 
 class Cpu:
     def __init__(self,instructionPointer,memory: Memory):
@@ -47,7 +45,44 @@ class Cpu:
         }
         self.zeroFlag=0
 
+#elif '[' in registerid and ']' in registerid:
+#           registerid=registerid.replace('[','').replace(']','')
 
+    def set_register_value(self,registerid,value):
+        if registerid in self.registers:
+            self.registers[registerid]=value
+        elif '[' in registerid and ']' in registerid:
+            registerid=registerid.replace('[','').replace(']','')
+            self.memory.memory[registerid]=value
     
-    
+    def get_register_value(self,registerid):
+        if registerid in self.registers:
+            try:
+                return value=self.registers[registerid]
+            except:
+                return None
+        elif '[' in registerid and ']' in registerid:
+            registerid=registerid.replace('[','').replace(']','')
+            return value=self.memory.memory[registerid]
+        else:
+            return registerid
+
+    def ALU_Unit(self,operation,arg1,arg2):
+        if arg1 in self.registers or arg2 in self.registers or ('[' in arg2 and ']'in arg2):
+            match operation:
+                case '+':
+                    set_register_value(arg2,get_register_value(arg1)+get_register_value(arg2))
+                case '-':
+                    set_register_value(arg2,get_register_value(arg1)-get_register_value(arg2))
+                case '*':
+                    set_register_value(arg2,get_register_value(arg1)*get_register_value(arg2))
+                case '/':
+                    set_register_value(arg2,get_register_value(arg1)/get_register_value(arg2))
+                case 'or':
+                    set_register_value(arg2,get_register_value(arg1)|get_register_value(arg2))
+                case 'and':
+                    set_register_value(arg2,get_register_value(arg1)&get_register_value(arg2))
+                case 'xor':
+                    set_register_value(arg2,get_register_value(arg1)^get_register_value(arg2))
         
+
